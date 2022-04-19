537e9714926a578fc330980354d1bd6d92502263
Mads Kristian Rosendal Højberg Bau-Madsen <mkbm@netcompany.com>
2022-04-19 13:37:56 +0200

    small fixes

--- app/src/main/java/com/google/blockly/android/webview/demo/Blocks/AbstractBlock.java
+++ app/src/main/java/com/google/blockly/android/webview/demo/Blocks/AbstractBlock.java
@@ -3,13 +3,20 @@ package com.google.blockly.android.webview.demo.Blocks;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.UUID;
+
 public abstract class AbstractBlock {
 
     protected String id;
     //Empty constructor for init without json
     protected AbstractBlock(){}
     public AbstractBlock(JSONObject json) throws JSONException {
-        this.id = json.getString("id");
+        if(json.has("id")){
+            this.id = json.getString("id");
+        }else{
+            this.id = UUID.randomUUID().toString();
+        }
+
         this.parseFromJson(json);
     };
 
